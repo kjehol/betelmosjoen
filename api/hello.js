a427139196cd7987e2cf6dce16dbcceaa782ed32
kjehol <kjetilaholen@gmail.com>
2025-04-28 18:57:27 +0200

    1.09

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-// api/hello.js
-export const config = { runtime: "edge" };
-
-export default async function handler(request) {
-  return new Response(JSON.stringify({ ok: true, message: "Hello from Vercel!" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
