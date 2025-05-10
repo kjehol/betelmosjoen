665487244d9c40178013318869cc42289029a633
kjehol <kjetilaholen@gmail.com>
2025-05-10 12:03:13 +0200

    Push55 tidskode 4

--- src/components/Velkommen.jsx
+++ src/components/Velkommen.jsx
@@ -166,6 +166,7 @@ export default function Velkommen() {
                    year: 'numeric',
                    hour: '2-digit',
                    minute: '2-digit',
+                   second: "2-digit",
                 })}
               </small>
             </li>
