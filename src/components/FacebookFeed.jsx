522286f866f391dbc33d57d5684f56d384c405ed
kjehol <kjetilaholen@gmail.com>
2025-05-15 16:05:59 +0200

    fb iframe 2

--- src/components/FacebookFeed.jsx
+++ src/components/FacebookFeed.jsx
@@ -22,12 +22,13 @@ export default function FacebookFeed() {
         className="fb-page"
         data-href="https://www.facebook.com/pinsekirken"
         data-tabs="timeline"
-        data-width="500"
+        data-width="100%"
         data-height=""
         data-small-header="true"
         data-adapt-container-width="true"
         data-hide-cover="false"
         data-show-facepile="false"
+        style={{ minWidth: 280, maxWidth: 600 }}
       >
         <blockquote
           cite="https://www.facebook.com/pinsekirken"
