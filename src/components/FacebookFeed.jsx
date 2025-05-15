ecb34c05836c587022ae9370a5ed7deae40de962
kjehol <kjetilaholen@gmail.com>
2025-05-15 16:01:30 +0200

    Fb iframe 1

--- src/components/FacebookFeed.jsx
+++ src/components/FacebookFeed.jsx
@@ -5,7 +5,7 @@ export default function FacebookFeed() {
     // Last inn Facebook SDK hvis det ikke allerede er der
     if (!window.FB) {
       const script = document.createElement("script");
-      script.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0";
+      script.src = "https://connect.facebook.net/no_NB/sdk.js#xfbml=1&version=v22.0";
       script.async = true;
       script.defer = true;
       script.crossOrigin = "anonymous";
@@ -17,14 +17,14 @@ export default function FacebookFeed() {
   }, []);
 
   return (
-    <div className="max-w-md w-full mx-auto my-8">
+    <div className="w-full flex justify-center my-8">
       <div
         className="fb-page"
         data-href="https://www.facebook.com/pinsekirken"
         data-tabs="timeline"
-        data-width=""
+        data-width="500"
         data-height=""
-        data-small-header="false"
+        data-small-header="true"
         data-adapt-container-width="true"
         data-hide-cover="false"
         data-show-facepile="false"
